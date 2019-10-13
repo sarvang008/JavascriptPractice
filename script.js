254b4db4ceb42214051dfc353e8aedb1477a35e3
sarvang008 <sarvang.m@somaiya.edu>
2019-10-13 21:11:35 +0530

    done game

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let door1=document.getElementById('door1');
 let door2=document.getElementById('door2');
 
 let door3=document.getElementById('door3');
+let startButton=document.getElementById('start');
 let openDoor1;
 let openDoor2;
 let openDoor3;
@@ -10,9 +11,37 @@ let closedoorpath='https://s3.amazonaws.com/codecademy-content/projects/chore-do
 let beachdoorpath='https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
 let botdoorpath='https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
 let spacedoorpath='https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
+let numcloseddoors;
+let currentlyplaying=false;
+const isclicked=(door)=>{
+  if(door.src===closedoorpath)
+  return true;
+  else
+  return false;
+
+}
+const isBot=(door)=>{
+  if(door.src===botdoorpath)
+  return true;
+  else
+  return false;
+
+}
+const playdoor=(door)=>
+{
+numcloseddoors--;
+if(numcloseddoors===0)
+{
+gameover('win');
+}
+else if(isBot(door))
+{
+  gameover('loose');
+}
+}
 const randomChoreDoorGenerator=()=>
 {
-const numcloseddoors=3;
+
 const choreDoor=Math.floor(Math.random()*numcloseddoors);
 console.log(choreDoor);
 
@@ -33,20 +62,62 @@ if(choreDoor === 0) {
     openDoor2 = spacedoorpath; 
   }
 }
-randomChoreDoorGenerator();
+
 
 door1.onclick=()=>{
     //door1.setAttribute('src',"https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg");
-door1.src=openDoor1;
-
+    if(isclicked(door1)&& currentlyplaying===true)
+    {
+      door1.src=openDoor1;
+      playdoor(door1);
+    }   
 
 }
 door2.onclick=()=>{
-    door2.src=openDoor2;
-    }
+  if(isclicked(door2)&& currentlyplaying===true)
+     { 
+        door2.src=openDoor2;
+          playdoor(door2);
+      }
+  }
 
     
 door3.onclick=()=>{
+  if(isclicked(door3)&& currentlyplaying===true)
+  {
     door3.src=openDoor3;
+    playdoor(door3);
+  }
     }
 
+    const gameover=(status)=>{
+
+      if(status==='win')
+      {
+        startButton.innerHTML='You Win!! Play Again';
+      }
+      else
+      {
+        startButton.innerHTML="You Loose!! Play Again";
+      }
+      currentlyplaying=false;
+
+    }
+startButton.onclick=()=>{
+  startround();
+}
+function startround()
+{
+  console.log('in startround');
+  if(currentlyplaying!==true)
+  {
+  door1.src=closedoorpath;
+  door2.src=closedoorpath;
+  door3.src=closedoorpath;
+  numcloseddoors=3;
+  currentlyplaying=true;
+  startButton.innerHTML='start';
+  randomChoreDoorGenerator();
+  }
+
+}
